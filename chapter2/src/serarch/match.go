95c61a29d6176dc86a324b0ecc9fd9eb66e7bbae
Telei <teilei_huang@163.com>
2018-08-17 19:19:19 +0800

    match.go

--- chapter2/src/serarch/match.go
+++ chapter2/src/serarch/match.go
@@ -2,6 +2,7 @@ package serarch
 
 import (
 	"log"
+	//"fmt"
 )
 
 type Result struct {
@@ -11,4 +12,24 @@ type Result struct {
 
 type Matcher interface {
 	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+func Macth(matcher Matcher, feed *Feed, searchTerm string, results chan<- *Result) {
+
+	serarchResults, err := matcher.Search(feed, searchTerm)
+
+	if err != nil {
+
+		log.Println(err)
+		return
+	}
+	for _, result := range serarchResults {
+		results <- result
+	}
+}
+
+func Display(results chan *Result) {
+	for result :=results{
+		fmt.Println("%s:\n%s\n\n",result.Filed,result.Content)
+	}
 }
